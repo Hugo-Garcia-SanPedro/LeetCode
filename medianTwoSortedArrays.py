f641fcec9078656c3f6a518e4adbfeb0debab5e4
Hugo-Garcia-SanPedro <garcia.sanpedro.hugo@gmail.com>
2026-06-28 19:51:02 +0200

    Changes in median two sorted arrays

--- medianTwoSortedArrays.py
+++ medianTwoSortedArrays.py
@@ -1,5 +1,6 @@
 from typing import List
 import math
+import statistics
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
@@ -20,22 +21,23 @@ class Solution:
                 finalArray[i + 1] = finalArray[i]
                 finalArray[i] = alternative
 
-        # Next we calculate the median
-        if ((n + m) % 2) == 0:
-            half1 = math.trunc((n + m) / 2)
-            half2 = half1 + 1
-            median = (half1 + half2) / 2
+        # Calculate the median
+        if (n + m) % 2 == 0:
+            mid = (n + m) // 2
+            number1 = finalArray[mid - 1]
+            number2 = finalArray[mid]
+            median = (number1 + number2) / 2
         else:
-            half = math.trunc((n + m) / 2)
-            median = finalArray[half + 1]
+            mid = (n + m) // 2
+            median = finalArray[mid]
 
         return median
 
 if __name__ == "__main__":
     solution = Solution()
 
-    nums1 = [1, 3]
-    nums2 = [2]
+    nums1 = [0, 0]
+    nums2 = [0, 0]
 
     median = solution.findMedianSortedArrays(nums1=nums1, nums2=nums2)
     print(f"The median of {nums1} and {nums2} are: {median}.")
