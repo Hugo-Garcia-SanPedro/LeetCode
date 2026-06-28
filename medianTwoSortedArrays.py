e79c9f2be3b3f4b69ab5eb7ddf64fb419935a533
Hugo-Garcia-SanPedro <garcia.sanpedro.hugo@gmail.com>
2026-06-28 19:25:26 +0200

    First probe of median two sorted arrays

--- medianTwoSortedArrays.py
+++ medianTwoSortedArrays.py
@@ -1,4 +1,5 @@
 from typing import List
+import math
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
@@ -6,15 +7,29 @@ class Solution:
         m = len(nums2)
         finalArray = []
 
-        # First we have to join in order the 2 arrays
+        # First we have to join the 2 arrays
         for i in range(n):
-            for j in range(m):
-                if nums1[i] < nums2[j]:
-                    finalArray.append(nums1[i])
-                else:
-                    finalArray.append(nums2[j])
+            finalArray.append(nums1[i])
+        for j in range(m):
+            finalArray.append(nums2[j])
 
-        return finalArray
+        # Next I order the array
+        for i in range((n + m) - 1):
+            if finalArray[i] > finalArray[i + 1]:
+                alternative = finalArray[i + 1]
+                finalArray[i + 1] = finalArray[i]
+                finalArray[i] = alternative
+
+        # Next we calculate the median
+        if ((n + m) % 2) == 0:
+            half1 = math.trunc((n + m) / 2)
+            half2 = half1 + 1
+            median = (half1 + half2) / 2
+        else:
+            half = math.trunc((n + m) / 2)
+            median = finalArray[half + 1]
+
+        return median
 
 if __name__ == "__main__":
     solution = Solution()
