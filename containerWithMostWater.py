2381202688cdc1b61de22395036c379a11b18693
Hugo-Garcia-SanPedro <garcia.sanpedro.hugo@gmail.com>
2026-06-29 11:40:56 +0200

    New problem: Container with the most water

---
+++ containerWithMostWater.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+
+    result = solution.maxArea(height=height)
+    print(f"The full array is: {height}, the output is: {result}.")
