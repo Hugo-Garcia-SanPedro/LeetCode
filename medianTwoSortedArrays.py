4277da016016dbd2707eb2f08e2824002a0d2512
Hugo-Garcia-SanPedro <garcia.sanpedro.hugo@gmail.com>
2026-06-29 11:30:25 +0200

    Median two sorted arrays solve

--- medianTwoSortedArrays.py
+++ medianTwoSortedArrays.py
@@ -15,29 +15,26 @@ class Solution:
             finalArray.append(nums2[j])
 
         # Next I order the array
-        for i in range((n + m) - 1):
-            if finalArray[i] > finalArray[i + 1]:
-                alternative = finalArray[i + 1]
-                finalArray[i + 1] = finalArray[i]
-                finalArray[i] = alternative
+        ordenated = sorted(finalArray)
+        order = len(ordenated)
 
         # Calculate the median
-        if (n + m) % 2 == 0:
-            mid = (n + m) // 2
-            number1 = finalArray[mid - 1]
-            number2 = finalArray[mid]
+        if (order) % 2 == 0:
+            mid = (order) // 2
+            number1 = ordenated[mid - 1 ]
+            number2 = ordenated[mid]
             median = (number1 + number2) / 2
         else:
             mid = (n + m) // 2
-            median = finalArray[mid]
+            median = ordenated[mid]
 
         return median
 
 if __name__ == "__main__":
     solution = Solution()
 
-    nums1 = [0, 0]
-    nums2 = [0, 0]
+    nums1 = [2, 5, 3]
+    nums2 = [9, 1, 5, 2]
 
     median = solution.findMedianSortedArrays(nums1=nums1, nums2=nums2)
     print(f"The median of {nums1} and {nums2} are: {median}.")
