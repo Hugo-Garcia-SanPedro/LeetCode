08816498c4ddfed9c8bb006847f044386da66de8
Hugo-Garcia-SanPedro <garcia.sanpedro.hugo@gmail.com>
2026-07-22 20:04:19 +0200

    Problem solved

--- Medium/removeNthNode.py
+++ Medium/removeNthNode.py
@@ -8,8 +8,21 @@ class ListNode:
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         dummy = ListNode(0, head)
-
-        return dummy
+        slow = fast = dummy
+        
+        # Move fast n+1 steps ahead
+        for _ in range(n + 1):
+            fast = fast.next
+        
+        # Move both until fast hits the end
+        while fast:
+            slow = slow.next
+            fast = fast.next
+        
+        # Remove the target node
+        slow.next = slow.next.next
+        
+        return dummy.next
 
 if __name__ == "__main__":
     solution = Solution()
