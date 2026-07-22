316ffbb83e92bf6e1e2b1047e400ec8af211f927
Hugo-Garcia-SanPedro <garcia.sanpedro.hugo@gmail.com>
2026-07-22 19:59:17 +0200

    Mofified in removeNthNode.py

--- Medium/removeNthNode.py
+++ Medium/removeNthNode.py
@@ -1,10 +1,15 @@
 from typing import Optional
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        head.delete(n)
+        dummy = ListNode(0, head)
 
-        return head
+        return dummy
 
 if __name__ == "__main__":
     solution = Solution()
