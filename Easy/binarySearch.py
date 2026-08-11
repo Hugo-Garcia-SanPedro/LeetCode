2926a1ee9543f68fb67d24d254f276cd6f502f30
Hugo-Garcia-SanPedro <garcia.sanpedro.hugo@gmail.com>
2026-08-11 21:28:24 +0200

    Finish the binary search algorithm

--- Easy/binarySearch.py
+++ Easy/binarySearch.py
@@ -1,9 +1,20 @@
 from typing import List
 
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        position = 0
-        return position
+    def search(self, nums: list[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
 
 if __name__ == "__main__":
     solution = Solution()
