90d5c2027d72908f3f436d789a6977b0b1a8e8fb
Hugo-Garcia-SanPedro <garcia.sanpedro.hugo@gmail.com>
2026-08-14 18:53:32 +0200

    Search insert position finish

--- Easy/searchInsertPosition.py
+++ Easy/searchInsertPosition.py
@@ -2,20 +2,22 @@ from typing import List
 
 def searchInsert(nums: List[int], target: int) -> int:
     inicio = 0
-    final = len(nums)
+    final = len(nums) - 1
 
     while inicio <= final:
-        medio = inicio + (final - inicio) // 2
+        medio = (inicio + final) // 2
         if nums[medio] == target:
             return medio
         elif nums[medio] < target:
-            inicio = nums[medio] + 1
+            inicio = medio + 1
         else:
-            final = nums[medio] - 1
+            final = medio - 1
+
+    return inicio
 
 if __name__ == "__main__":
-    numeros = [1, 3, 4, 6]
-    objetivo = 4
+    numeros = [1, 3, 5, 6]
+    objetivo = 5
 
     posicion = searchInsert(nums=numeros, target=objetivo)
     print(f"El objetivo: {objetivo}, en el array: {numeros}, se introduce en la posicion: {posicion}.")
