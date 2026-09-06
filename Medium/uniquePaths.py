06f72110f0abcf3ad6e0af354950afce6e67c09e
Hugo-Garcia-SanPedro <garcia.sanpedro.hugo@gmail.com>
2026-09-06 17:53:49 +0200

    I make the skeleton of the backtrack function

--- Medium/uniquePaths.py
+++ Medium/uniquePaths.py
@@ -1,5 +1,26 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        # Fist we make the grid
+        grid = [["."] * n for i in range(m)]
+        res = []
+
+        # Backtracking
+        def backtrack(row):
+            if row == m:
+                copy = ["".join(rowGrid) for rowGrid in grid]
+                res.append(copy)
+                return
+
+            for column in range(n):
+                if self.isValid(row, column, grid):
+                    grid[row][column] = "X"
+                    backtrack(row + 1)
+                    grid[row][column] = "."
+
+        backtrack(0)
+        return len(res)
+
+    def isValid(n: int, m: int, grid):
 
 if __name__ == "__main__":
     solutions = Solution()
