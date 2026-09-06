907b465ab9b8f51df44d829307ab53820eddd402
Hugo-Garcia-SanPedro <garcia.sanpedro.hugo@gmail.com>
2026-09-06 17:35:30 +0200

    Fist I did the main

--- Medium/uniquePaths.py
+++ Medium/uniquePaths.py
@@ -1,2 +1,9 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+
+if __name__ == "__main__":
+    solutions = Solution()
+    m = 3
+    n = 7
+    output = solutions.uniquePaths(m=m, n=n)
+    print(f"Las posibles soluciones son: {output}.")
