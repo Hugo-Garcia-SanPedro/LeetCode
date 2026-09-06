89138f348705f4bb2819f019005118bc761a940b
Hugo-Garcia-SanPedro <garcia.sanpedro.hugo@gmail.com>
2026-09-06 16:56:37 +0200

    Def backtrack in n-Queens2

--- Hard/n-Queens2.py
+++ Hard/n-Queens2.py
@@ -2,3 +2,26 @@ from typing import List
 
 class Solution:
     def totalNQueens(self, n: int) -> int:
+        # Fist the variable we need
+        solutions = 0
+        res = []
+        board = [["."] * n for i in range(n)]
+
+        def backtrack(row):
+            # Next the base problem
+            if row == n:
+                copy = ["".join(rowBoard) for rowBoard in board]
+                res.append(copy)
+                solutions = solutions + 1
+                return
+
+            for column in range(n):
+                if self.isSafe(row, column, board):
+                    # Fisrt we move the queen
+                    board[row][column] = "Q"
+                    # Second we do the backtracking
+                    backtrack(row + 1)
+                    board[row][column] = "."
+
+        backtrack(0)
+        return solutions
---
+++ Medium/uniquePaths.py
@@ -0,0 +1,1 @@
+def uniquePaths(m: int, n: int) -> int:
